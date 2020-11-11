ede310c12efb8f5d79338806663a0dddab9c2e11
geonho kim <geonho_kim2@tmax.co.kr>
2020-11-11 17:37:17 +0900

    added docker daemon exec logic and applied to admission

--- pkg/server/admission.go
+++ pkg/server/admission.go
@@ -1,17 +1,31 @@
 package server
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
 
+	"github.com/eddy-kor-92/image-webhook/internal/k8s"
 	"k8s.io/api/admission/v1beta1"
 	core "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+const (
+	dndPod       = "dnd-pod"
+	dndContainer = "dnd-container"
+	dndNamespace = "default"
+)
+
 // ImageValidationAdmission is ...
 type ImageValidationAdmission struct {
+}
+
+// ExecResult is ...
+type ExecResult struct {
+	OutBuffer *bytes.Buffer
+	ErrBuffer *bytes.Buffer
 }
 
 // HandleAdmission is ...
@@ -25,18 +39,21 @@ func (*ImageValidationAdmission) HandleAdmission(review *v1beta1.AdmissionReview
 	}
 
 	isValid := true
-	name := "default image" // testing code
+	name := "default image"
 
 	for _, container := range pod.Spec.Containers {
-		name = container.Image // testing code
-		log.Println(name)
 		isValid = isValid && isSignedImage(container.Image)
+
+		if !isValid {
+			name = container.Image
+			break
+		}
 	}
 
 	review.Response = &v1beta1.AdmissionResponse{
 		Allowed: isValid,
 		Result: &v1.Status{
-			Message: name,
+			Message: fmt.Sprintf("Image '%s' is not signed", name),
 		},
 	}
 
@@ -44,5 +61,18 @@ func (*ImageValidationAdmission) HandleAdmission(review *v1beta1.AdmissionReview
 }
 
 func isSignedImage(image string) bool {
-	return true
+	result := &ExecResult{
+		OutBuffer: &bytes.Buffer{},
+		ErrBuffer: &bytes.Buffer{},
+	}
+
+	command := fmt.Sprintf("docker trust inspect %s", image)
+
+	if err := k8s.ExecCmd(dndPod, dndContainer, dndNamespace, command, nil, result.OutBuffer, result.ErrBuffer); err != nil {
+		log.Panicf("Failed to execute command to docker daemon by %s", err)
+	}
+
+	log.Println(result.OutBuffer.String())
+
+	return false
 }
