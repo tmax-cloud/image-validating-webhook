951c10208c0ca28f20f5fa061c9651ede734bfc5
geonho kim <16secondlife@gmail.com>
2020-11-26 16:21:56 +0900

    modify script for generating certificate

--- deploy/webhook-create-signed-cert.sh
+++ deploy/webhook-create-signed-cert.sh
@@ -4,48 +4,9 @@
 
 set -e
 
-usage() {
-    cat <<EOF
-Generate certificate suitable for use with an sidecar-injector webhook service.
-This script uses k8s' CertificateSigningRequest API to a generate a
-certificate signed by k8s CA suitable for use with sidecar-injector webhook
-services. This requires permissions to create and approve CSR. See
-https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster for
-detailed explanation and additional instructions.
-The server key/cert k8s CA cert are stored in a k8s secret.
-usage: ${0} [OPTIONS]
-The following flags are required.
-       --service          Service name of webhook.
-       --namespace        Namespace where webhook service and secret reside.
-       --secret           Secret name for CA certificate and server certificate/key pair.
-EOF
-    exit 1
-}
-
-while [[ $# -gt 0 ]]; do
-    case ${1} in
-        --service)
-            service="$2"
-            shift
-            ;;
-        --secret)
-            secret="$2"
-            shift
-            ;;
-        --namespace)
-            namespace="$2"
-            shift
-            ;;
-        *)
-            usage
-            ;;
-    esac
-    shift
-done
-
-[ -z "${service}" ] && service=image-validation-admission-svc
-[ -z "${secret}" ] && secret=image-validation-admission-certs
-[ -z "${namespace}" ] && namespace=registry-system
+service=image-validation-admission-svc
+secret=image-validation-admission-certs
+namespace=registry-system
 
 if [ ! -x "$(command -v openssl)" ]; then
     echo "openssl not found"
