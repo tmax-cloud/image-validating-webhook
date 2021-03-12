92cb84948687b7fa073e2bddafdd9ee693406115
geonho <16secondlife@gmail.com>
2021-03-12 17:36:57 +0900

    add digest to spec.container.image when it's signed image

--- pkg/server/admission.go
+++ pkg/server/admission.go
@@ -24,6 +24,12 @@ type ImageValidationAdmission struct {
 	client *kubernetes.Clientset
 }
 
+type patchOperation struct {
+	Op    string      `json:"op"`
+	Path  string      `json:"path"`
+	Value interface{} `json:"value,omitempty"`
+}
+
 // HandleAdmission is ...
 func (a *ImageValidationAdmission) HandleAdmission(review *v1beta1.AdmissionReview) error {
 	pod := core.Pod{}
@@ -52,17 +58,36 @@ func (a *ImageValidationAdmission) HandleAdmission(review *v1beta1.AdmissionRevi
 		return err
 	}
 
-	isValid := handler.isNamespaceInWhiteList()
-	var name = ""
-	if !isValid {
-		log.Println("INFO: This pod's namespace is not in the white list")
-		isValid, name = handler.isValid()
-	}
-
-	if isValid {
-		log.Println("INFO: Pod is valid")
+	if handler.isNamespaceInWhiteList() {
+		log.Println("INFO: This pod's namespace is in the white list")
 		review.Response = &v1beta1.AdmissionResponse{
 			Allowed: true,
+		}
+
+		return nil
+	}
+
+	isValid, name := handler.isValid()
+	if isValid {
+		log.Println("INFO: Pod is valid")
+		patch, err := createPatch(handler.GetPatch())
+		if err != nil {
+			log.Printf("ERROR: Couldn't make patched pod by %s", err)
+			review.Response = &v1beta1.AdmissionResponse{
+				Allowed: false,
+				Result: &v1.Status{
+					Message: fmt.Sprintf("Internal webhook server error: %s", err),
+				},
+			}
+
+			return err
+		}
+
+		patchType := v1beta1.PatchTypeJSONPatch
+		review.Response = &v1beta1.AdmissionResponse{
+			Allowed:   true,
+			Patch:     patch,
+			PatchType: &patchType,
 		}
 	} else {
 		log.Println("INFO: Pod is invalid")
@@ -75,4 +100,28 @@ func (a *ImageValidationAdmission) HandleAdmission(review *v1beta1.AdmissionRevi
 	}
 
 	return nil
+}
+
+func createPatch(patchPod *core.Pod) ([]byte, error) {
+	if patchPod == nil {
+		return nil, fmt.Errorf("Couldn't create patch")
+	}
+
+	patch := []patchOperation{
+		{
+			Op:    "replace",
+			Path:  "/spec/containers",
+			Value: patchPod.Spec.Containers,
+		},
+	}
+
+	if len(patchPod.Spec.InitContainers) > 0 {
+		patch = append(patch, patchOperation{
+			Op:    "replace",
+			Path:  "/spec/initContainers",
+			Value: patchPod.Spec.InitContainers,
+		})
+	}
+
+	return json.Marshal(&patch)
 }
